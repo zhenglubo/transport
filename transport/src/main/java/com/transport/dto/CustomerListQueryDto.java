2d72ffe7f9c256bae1dc2244257b57e2d9d91aab
zlb <1003966760@qq.com>
2019-04-06 17:23:01 +0800

    生成customer 实体类

---
+++ transport/src/main/java/com/transport/dto/CustomerListQueryDto.java
@@ -0,0 +1,23 @@
+package com.transport.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * Created by zlb on 2019.04.02.
+ */
+@Data
+@ApiModel("分页查询Dto")
+public class CustomerListQueryDto {
+
+    @ApiModelProperty(name = "用户名",example = "王林")
+    private String name;
+    @ApiModelProperty(name = "联系电话",position = 1,example = "15674518896")
+    private String phoneNumber;
+    @ApiModelProperty(name = "当前页",position = 2,example = "0",required = true)
+    private int current;
+    @ApiModelProperty(name = "每一页大小",position = 3,example = "10",required = true)
+    private int size;
+
+}
